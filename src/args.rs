d1eeb866d82554b4f6e5732c888f6967622e60c0
ZodiacalComet <ZodiacalComet@gmail.com>
2022-01-18 22:49:06 -0400

    Set some names for values in CLI arguments

--- src/args.rs
+++ src/args.rs
@@ -25,18 +25,18 @@ pub struct Args {
     #[clap(short, long, forbid_empty_values(true))]
     pub author: String,
     /// Cover image to use.
-    #[clap(short, long, forbid_empty_values(true))]
+    #[clap(short, long, value_name = "FILE", forbid_empty_values(true))]
     pub cover: Option<String>,
     /// Greentext subjects/tags.
     ///
     /// Can be used multiple times to set more than one.
-    #[clap(short, long = "subject", forbid_empty_values(true))]
+    #[clap(short, long = "subject", value_name = "SUBJECT", forbid_empty_values(true))]
     pub subjects: Option<Vec<String>>,
     /// Color of the green highlight.
-    #[clap(long, default_value = "#2CAF26", forbid_empty_values(true))]
+    #[clap(long, default_value = "#2CAF26", value_name = "COLOR", forbid_empty_values(true))]
     pub green_color: String,
     /// Color of the spoiler highlight.
-    #[clap(long, default_value = "#000", forbid_empty_values(true))]
+    #[clap(long, default_value = "#000", value_name = "COLOR", forbid_empty_values(true))]
     pub spoiler_color: String,
     /// Supress all output.
     #[clap(short, long, conflicts_with("verbose"))]
@@ -45,12 +45,12 @@ pub struct Args {
     #[clap(short, long, parse(from_occurrences))]
     pub verbose: usize,
     /// When to use colors.
-    #[clap(long, arg_enum, default_value_t)]
+    #[clap(long, arg_enum, default_value_t, value_name = "WHEN")]
     pub color: Color,
     /// Path for the generated epub file.
-    #[clap(short, long, forbid_empty_values(true))]
+    #[clap(short, long, value_name = "PATH", forbid_empty_values(true))]
     pub output: String,
     /// Text files in greentext format to convert.
-    #[clap(forbid_empty_values(true))]
+    #[clap(value_name = "FILE", forbid_empty_values(true))]
     pub files: Vec<String>,
 }
