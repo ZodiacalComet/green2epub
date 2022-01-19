c13510bebbad8dc228c2204debcd6c9b3ce3842d
ZodiacalComet <ZodiacalComet@gmail.com>
2022-01-18 23:26:33 -0400

    Apply `cargo fmt`

--- src/args.rs
+++ src/args.rs
@@ -25,18 +25,43 @@ pub struct Args {
     #[clap(short, long, display_order = 2, forbid_empty_values(true))]
     pub author: String,
     /// Cover image to use.
-    #[clap(short, long, value_name = "FILE", display_order = 3, forbid_empty_values(true))]
+    #[clap(
+        short,
+        long,
+        value_name = "FILE",
+        display_order = 3,
+        forbid_empty_values(true)
+    )]
     pub cover: Option<String>,
     /// Greentext subjects/tags.
     ///
     /// Can be used multiple times to set more than one.
-    #[clap(short, long = "subject", alias = "tag", value_name = "SUBJECT", display_order = 4, forbid_empty_values(true))]
+    #[clap(
+        short,
+        long = "subject",
+        alias = "tag",
+        value_name = "SUBJECT",
+        display_order = 4,
+        forbid_empty_values(true)
+    )]
     pub subjects: Option<Vec<String>>,
     /// Color of the green highlight.
-    #[clap(long, default_value = "#2CAF26", value_name = "COLOR", display_order = 6, forbid_empty_values(true))]
+    #[clap(
+        long,
+        default_value = "#2CAF26",
+        value_name = "COLOR",
+        display_order = 6,
+        forbid_empty_values(true)
+    )]
     pub green_color: String,
     /// Color of the spoiler highlight.
-    #[clap(long, default_value = "#000", value_name = "COLOR", display_order = 7, forbid_empty_values(true))]
+    #[clap(
+        long,
+        default_value = "#000",
+        value_name = "COLOR",
+        display_order = 7,
+        forbid_empty_values(true)
+    )]
     pub spoiler_color: String,
     /// Shows verbose output, can be used multiple times to set level of verbosity.
     #[clap(short, long, display_order = 8, parse(from_occurrences))]
@@ -45,10 +70,22 @@ pub struct Args {
     #[clap(short, long, display_order = 9, conflicts_with("verbose"))]
     pub quiet: bool,
     /// When to use colors.
-    #[clap(long, arg_enum, default_value_t, display_order = 10, value_name = "WHEN")]
+    #[clap(
+        long,
+        arg_enum,
+        default_value_t,
+        display_order = 10,
+        value_name = "WHEN"
+    )]
     pub color: Color,
     /// Path for the generated epub file.
-    #[clap(short, long, value_name = "PATH", display_order = 5, forbid_empty_values(true))]
+    #[clap(
+        short,
+        long,
+        value_name = "PATH",
+        display_order = 5,
+        forbid_empty_values(true)
+    )]
     pub output: String,
     /// Text files in greentext format to convert.
     #[clap(value_name = "FILE", forbid_empty_values(true))]
